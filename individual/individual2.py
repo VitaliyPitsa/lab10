10ccdf112b2808e6991b001fd9b2d056829109bb
VitaliyPitsa <99473215+VitaliyPitsa@users.noreply.github.com>
2022-12-02 21:26:51 +0300

    fix

---
+++ individual/individual2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+if __name__ == '__main__':
+    file_name = input("Введите имя файла: ")
+    os.rename("ind2.txt", file_name)
+    with open(file_name, 'r') as f:
+        line = f.readline().lower()
+        if line:
+            line = line.split()
+            s = {}
+            i1 = 0
+            min1 = 0
+            for i in line:
+                if i not in s:
+                    s[i] = 1
+                else:
+                    s[i] += 1
+            for values in s.values():
+                if values > i1:
+                     i1 = values
+            for keys, values in s.items():
+                if values == i1:
+                     min1 = keys
+            print(min1)
