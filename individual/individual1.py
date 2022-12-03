337df30ebfa179d7039c94ce2d1b4e065eadcd98
VitaliyPitsa <99473215+VitaliyPitsa@users.noreply.github.com>
2022-12-03 09:11:57 +0300

    fix

---
+++ individual/individual1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    with open('text.txt', 'r') as f:
+        text = f.read()
+    k = 0
+    m = 0
+    for i in text:
+        if i.isalpha():
+            m += 1
+        else:
+            if m > 7:
+                k += 1
+            m = 0
+    print(k)
